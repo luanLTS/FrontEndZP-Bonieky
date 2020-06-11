4935ff00d9a6d8a1f8e8943b197068405a033c2c
Luan Teixeira <luanlts@outlook.com>
2020-06-11 15:38:18 -0300

    first commit

---
+++ Modulo 1 - HTML e CSS/main.js
@@ -0,0 +1,9 @@
+function teste1 (){
+    alert("voce clicou no botao")
+}
+function teste2 (elemento){
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+function teste3 (elemento){
+    elemento.innerHTML = "Passou o mouse"
+}
